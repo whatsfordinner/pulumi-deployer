f4d028c9c5ca9aefd1a37310efb3b4f6e0d39070
what's for dinner <cruelangeleo7@gmail.com>
2023-12-22 23:10:51 +1100

    finished the base infra stack

--- infra/Program.cs
+++ infra/Program.cs
@@ -39,9 +39,63 @@ return await Deployment.RunAsync(() =>
         }),
     });
 
+    var jobPolicy = new Pulumi.Aws.Iam.Policy("jobPolicy", new()
+    {
+        Path = "/jobdoer/",
+        NamePrefix = "jobExecutionPolicy",
+        PolicyDocument = Output.Tuple(bucket.Arn, queue.Arn)
+        .Apply(t =>
+        {
+            string bucketArn = t.Item1;
+            string queueArn = t.Item2;
+
+            var policy = new Dictionary<string, object?>
+            {
+                ["Version"] = "2012-10-17",
+                ["Statement"] = new[]
+                {
+                    new Dictionary<string, object?>
+                    {
+                        ["Action"] = new[]
+                        {
+                            "s3:Get*",
+                            "s3:Put*",
+                            "s3:DeleteObject",
+                        },
+                        ["Effect"] = "Allow",
+                        ["Resource"] = bucketArn,
+                    },
+                    new Dictionary<string, object?>
+                    {
+                        ["Action"] = new[]
+                        {
+                            "sqs:ReceiveMessage",
+                            "sqs:DeleteMessage",
+                        },
+                        ["Effect"] = "Allow",
+                        ["Resource"] = queueArn,
+                    },
+                }
+            };
+
+            return JsonSerializer.Serialize(policy);
+        }),
+    });
+
+    var jobPolicyAttachment = new Pulumi.Aws.Iam.RolePolicyAttachment("jobPolicyAttachment", new()
+    {
+        PolicyArn = jobPolicy.Arn,
+        Role = jobRole.Name,
+    });
+
     // Export outputs here
     return new Dictionary<string, object?>
     {
-        ["outputKey"] = "outputValue"
+        ["jobRole"] = jobRole.Name,
+        ["environment"] = new Dictionary<string, object?>
+        {
+            ["JOB_QUEUE_URL"] = queue.Url,
+            ["JOB_BUCKET_NAME"] = bucket.Id,
+        }
     };
 });
